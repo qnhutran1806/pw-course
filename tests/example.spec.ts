0701accaa9be6c1cf463934a8280600a6ee9b309
Nhu <qnhutran1806@gmail.com>
2025-03-25 09:32:47 +0700

    init project

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://material.playwrightvn.com/');
 
-  // Expect a title "to contain" a substring. //change
+  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Tài liệu học automation test/);
 });
 
